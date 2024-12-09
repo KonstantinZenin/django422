871d28de57176bf62a397b2a1c3261bba687e8f2
Konstantin <Kostick_zenin@mai.ru>
2024-12-09 19:57:52 +0300

    создали первые пути

--- python_blog/urls.py
+++ python_blog/urls.py
@@ -14,16 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('djangoProject/', include('djangoProject.urls'))
 """
-# Файл djangoProject
+import python_blog.urls
+
+# Файл из python_blog.urls
 from django.contrib import admin
 from django.urls import path
-from python_blog.views import main
-from django.urls import include
+from python_blog.views import catalog_posts, post_detail, catalog_categories, category_datail, catalog_tags, tag_detail
 
-# Общий префикс posts/
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', main),
-    # Подключаем python_blog.urls
-    path('posts/', include('python_blog.urls'))
+    # категории
+    path('categories/', catalog_categories),
+    path('categories/<slug:categories_slug>', category_datail),
+
+    # Теги
+    path('tags', catalog_tags),
+    path('tags/<slug:tags_slug>', tag_detail),
+
+    # посты posts/tags
+    path('', catalog_posts),
+    path('<slug:post_slug>', post_detail),  # /posts/osnovy-python/
 ]
--- python_blog/views.py
+++ python_blog/views.py
@@ -2,6 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def main(request, name):
-    string = f"Привет {name}!"
-    return HttpResponse(string)
+def main(request):
+    return HttpResponse('Главная страница')
+
+
+def catalog_posts(request):
+    return HttpResponse('Каталог постов')
+
+
+def post_detail(request, post_slug):
+    return HttpResponse(f'Страница поста {post_slug}')
+
+
+def catalog_categories(request):
+    return HttpResponse(f'Каталог категорий')
+
+
+def category_datail(request, categories_slug):
+    return HttpResponse(f'Страница категории {categories_slug}')
+
+
+def catalog_tags(request):
+    return HttpResponse(f'Каталог тегов')
+
+
+def tag_detail(request, tags_slug):
+    return HttpResponse(f'Страница тега {tags_slug}')
