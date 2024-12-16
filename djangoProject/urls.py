9ce540046d706f46269be1c187b800f42f536f93
Konstantin <Kostick_zenin@mai.ru>
2024-12-16 20:50:58 +0300

    lesson_56 подключили статику

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,6 +16,8 @@ from django.contrib import admin
 from django.urls import path
 from python_blog.views import main
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Общий префикс posts/
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     path('', main, name="main"),
     # Подключаем python_blog.urls
     path('posts/', include('python_blog.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
