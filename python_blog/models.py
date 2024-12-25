828a173243cf70bbea98f4556c1f1f8ba89901bd
Konstantin <Kostick_zenin@mai.ru>
2024-12-25 18:57:32 +0300

    lesson_59 один ко многим и модель категории

--- python_blog/models.py
+++ python_blog/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
+
 class Post(models.Model):
     title = models.CharField(max_length=100, unique=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    category = models.CharField(null=True, blank=True, max_length=100)
+    category = models.ForeignKey(
+        'Category',  # Ссылка на модель Category
+        on_delete=models.SET_DEFAULT,  # При удалении категории, установить значение NULL
+        blank=True,  # Не требуем в формах заполнения
+        null=True,  # Разрешаем значение NULL в базе данных
+        related_name="posts",  # Имя обратно связи
+        default=None
+    )
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True, null=True, default="Без описания")
+
 
 # PRICTICE - Работа с моделью Post
 """
